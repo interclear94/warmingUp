77cf4a0603b6ef4b378b1257ad210211feb141b3
dpclfk <lee@192.168.nate.com>
2024-12-17 16:55:40 +0900

    fix:search

--- be/services/list.ts
+++ be/services/list.ts
@@ -1,11 +1,17 @@
-import { Request, Response } from "express";
+import { Request, Response, urlencoded } from "express";
 import { Board } from "../model";
 import QueryString from "qs";
+import { Op } from "sequelize";
+
+interface Search {
+  title?: { [Op.like]: string };
+  user?: { [Op.like]: string };
+}
 
 export default async (req: Request, res: Response) => {
   try {
     const nowquery: QueryString.ParsedQs = req.query;
-    const search: { title?: string; user?: string } = {};
+    const search: Search = {};
     let nowpage: number = 1;
 
     if (+req.params.list > 1) {
@@ -13,16 +19,16 @@ export default async (req: Request, res: Response) => {
     }
 
     if (nowquery.title) {
-      search.title = String(nowquery.title);
+      search.title = { [Op.like]: `%${String(nowquery.title)}%` };
     } else if (nowquery.user) {
-      search.user = String(nowquery.user);
+      search.user = { [Op.like]: `%${String(nowquery.user)}%` };
     }
 
     const list: { rows: Board[]; count: number } = await Board.findAndCountAll({
-      where: search,
+      where: { ...search },
       attributes: { exclude: ["updatedAt", "deletedAt", "pw"] },
-      offset: (nowpage - 1) * 10,
-      limit: 10,
+      offset: (nowpage - 1) * 8,
+      limit: 8,
       order: [["id", "desc"]],
     });
 
