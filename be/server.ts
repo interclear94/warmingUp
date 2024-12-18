dc91ce8b4333936dc22491b928bf9d708a41cc10
dpclfk <lee@iseungbaeui-MacBookAir.local>
2024-12-18 22:41:40 +0900

    fix:controllers

--- be/server.ts
+++ be/server.ts
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5500",
+    origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
     credentials: true,
   })
 );
