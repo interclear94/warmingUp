bf999466115f0d5c07bb9a0a0eb3d01d4468faad
cheol <https://github.com/interclear94>
2024-12-20 13:39:21 +0900

    delete detail 합침

--- fe/js/detail.js
+++ fe/js/detail.js
@@ -3,6 +3,28 @@ const url = new URL(urlStr);
 const urlParams = url.searchParams;
 const numIndex = urlParams.get("page");
 
+const modalPopup = document.querySelector(".modal-popup");
+const deleteBtn = document.getElementById("deleteBtn");
+const cancelBtn = document.getElementById("cancelBtn");
+const confirmBtn = document.getElementById("confirmBtn");
+
+const passwordValue = document.getElementById("pw").value;
+
+deleteBtn.onclick = () => {
+  console.log("확인 하고 있어요");
+  // const data = await axios.get();
+
+  modalPopup.style.display = "block";
+  // confirmBtn.onclick = () => {
+  //   if (passwordValue)
+  //     axios.delete(`http://localhost:3000/api/boards/${numIndex}`);
+  // };
+};
+
+cancelBtn.onclick = () => {
+  modalPopup.style.display = "none";
+};
+
 const userName = document.querySelector(".user_info");
 const titleSpan = document.querySelector(".title > span");
 const contentDiv = document.querySelector("#content");
